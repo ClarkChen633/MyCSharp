8a0248fc2ce4b62c06e3595d53e1aa36e6f50d31
clarkchen633 <clarkchen633@gmail.com>
2016-05-26 09:44:55 +0800

    DLL smaple function.

---
+++ func/func/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func
+{
+    public class Class1
+    {
+        public static double addup(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double minus(double number1, double number2)
+        {
+            return number2 - number1;
+        }
+    }
+}
---
+++ func/funcexe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using func;
+
+namespace funcexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("addup :{0:G}",Class1.addup(1,2));
+            Console.WriteLine("minus :{0:G}", Class1.minus(1,2));
+            Console.ReadKey();
+        }
+    }
+}
